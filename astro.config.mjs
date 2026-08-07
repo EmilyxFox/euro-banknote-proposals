2e78dcddca87695795b1d749c11721fe06f42621
EmilyxFox <48589793+EmilyxFox@users.noreply.github.com>
2026-08-08 01:49:40 +0200

    add github pages config and action

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte()],
+  site: "https://emilyxfox.github.io",
+  base: "euro-banknote-proposals",
 
   vite: {
     plugins: [tailwindcss()]
